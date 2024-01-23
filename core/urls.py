4425eab6c4722ac52d217508bb1edeb6c94c4f20
Ricardo Santos <richard7santos@hotmail.com>
2024-01-23 17:08:17 -0300

    Cadastro de Usuários

--- core/urls.py
+++ core/urls.py
@@ -20,11 +20,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-from core.views import home
+from core.views import UserCreate, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', UserCreate.as_view(), name='registrar'),
+
     path('', home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
