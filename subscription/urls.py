b1c2a82478bd6deae35f8f82f0b5d03be795fd75
Ricardo Santos <richard7santos@hotmail.com>
2024-02-15 18:24:14 -0300

    Admin

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from subscription.views import add_pagamento, delete_incricao, detalhe_incricao, edit_incricao, list_incricao, add_incricao, pagamento_delete, pagamento_edit
+from subscription.views import add_pagamento, delete_incricao, detalhe_incricao, edit_incricao, list_incricao, add_incricao, pagamento_delete
 
 urlpatterns = [
     path('', add_incricao, name='inscrever'),
@@ -7,8 +7,7 @@ urlpatterns = [
     path('list/', list_incricao, name='incricoes_list'),
     path('delete/<int:id>', delete_incricao, name='incricoes_delete'),
     path('pagar/<int:id>', add_pagamento, name='incricoes_pagar'),
-    path('datalhe/<int:id>', detalhe_incricao, name='incricoes_detalhe'),
+    path('detalhe/<int:id>', detalhe_incricao, name='incricoes_detalhe'),
 
-    path('edit/pgto/<int:id>', pagamento_edit, name='pagamento_edit'),
     path('delete/pgto/<int:id>', pagamento_delete, name='pagamento_delete'),
 ]
